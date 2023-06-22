043db673847547bfcf723757da5535585424d9b0
Kraemer <jkremer952@gmail.com>
2023-06-22 13:43:55 -0400

    Add files via upload
    Function to check if a string is included in a matrix of strings. Required for AnalysisReport.m

---
+++ isincluded.m
@@ -0,0 +1,11 @@
+function output = isincluded(input, table)
+  rows = size(table,2);
+  found = false;
+  for check = 1:rows
+    if (strcmp(input,table(check)))
+      found = check;
+      break;
+    endif
+  endfor
+  output = found;
+
